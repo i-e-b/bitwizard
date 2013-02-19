cdd0f35bad580f7762c070def684b0ec05c2ebf2
Iain Ballard <iain.ballard@7digital.com>
2013-02-19 14:20:02 +0000

    tests

--- src/Unit.Tests/BitReaderTests.cs
+++ src/Unit.Tests/BitReaderTests.cs
@@ -1,4 +1,5 @@
-﻿using BitWizard;
+﻿using System;
+using BitWizard;
 using NUnit.Framework;
 
 namespace Unit.Tests
@@ -12,7 +13,7 @@ namespace Unit.Tests
 	    [SetUp]
         public void setup()
         {
-            _data = new byte[] { 0x81 };
+            _data = new byte[] { 0x82, 0xFF, 0xFF };
             _subject = new BitReader(_data);
         }
 
@@ -20,11 +21,28 @@ namespace Unit.Tests
         public void can_read_a_sequence_of_bits_as_boolean_values ()
         {
             Assert.That(_subject.GetFlag(), Is.True);
-	        for (int i = 0; i < 6; i++)
+	        for (int i = 0; i < 5; i++)
 	        {
                 Assert.That(_subject.GetFlag(), Is.False);
 	        }
             Assert.That(_subject.GetFlag(), Is.True);
+            Assert.That(_subject.GetFlag(), Is.False);
+        }
+
+        [Test]
+        public void can_read_a_byte_aligned_UInt16 ()
+        {
+            _subject.SkipBytes(1);
+            var result = (UInt16)_subject.GetInteger(16);
+            Assert.That(result, Is.EqualTo(0xFFFF));
+        }
+
+        [Test]
+        public void can_read_a_byte_unaligned_UInt16 ()
+        {
+            _subject.SkipBits(7);
+            var result = (UInt16)_subject.GetInteger(16);
+            Assert.That(result, Is.EqualTo(0x7FFF));
         }
     }
 }
