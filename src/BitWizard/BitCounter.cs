bc9ba38ee8e40b41d83371863caf124176008c1b
Iain Ballard <iain.ballard@7digital.com>
2013-02-26 15:41:47 +0000

    Counting bits

---
+++ src/BitWizard/BitCounter.cs
@@ -0,0 +1,24 @@
+﻿namespace BitWizard
+{
+	public static class BitCounter
+	{
+		public static int Count(int src)
+		{
+			src = src - ((src >> 1) & 0x55555555);
+			src = (src & 0x33333333) + ((src >> 2) & 0x33333333);
+			return ((src + (src >> 4) & 0xF0F0F0F) * 0x1010101) >> 24;
+		}
+		public static int Count(ulong src)
+		{
+			const ulong a = (ulong.MaxValue / 3);
+			const ulong b = ((ulong.MaxValue / 15) * 3);
+			const ulong c = ((ulong.MaxValue / 255) * 15);
+			const ulong d = (ulong.MaxValue / 255);
+
+			src = src - ((src >> 1) & a);
+			src = (src & b) + ((src >> 2) & b);
+			src = (src + (src >> 4)) & c;
+			return (int)(src * (d)) >> (sizeof(ulong) - 1) * 8;
+		}
+	}
+}
---
+++ src/Unit.Tests/BitCountingTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using BitWizard;
+using NUnit.Framework;
+
+namespace Unit.Tests
+{
+	[TestFixture]
+	public class BitCountingTests_Int
+	{
+		int[] cases;
+		int[] count;
+		const int SampleCount = 100;
+
+		[SetUp]
+		public void setup()
+		{
+			var rnd = new Random();
+			cases = new int[SampleCount];
+			count = new int[SampleCount];
+			cases[0] = 0;
+			count[0] = 0;
+			cases[1] = -1;
+			count[1] = 32;
+			for (int i = 2; i < SampleCount; i++)
+			{
+				cases[i] = rnd.Next();
+				count[i] = SimpleCount(cases[i]);
+			}
+		}
+
+		int SimpleCount(int src)
+		{
+			var c = 0;
+			for (int i = 0; i < 64; i++)
+			{
+				if (src % 2 == 1) c++;
+				src >>= 1;
+			}
+			return c;
+		}
+
+		[Test]
+		public void can_count_bits_in_an_int_with_efficient_method()
+		{
+			for (int i = 0; i < SampleCount; i++)
+			{
+				Console.WriteLine(cases[i].ToString("X") + " -> " + count[i]);
+				Assert.That(BitCounter.Count(cases[i]), Is.EqualTo(count[i]));
+			}
+		}
+		
+		[Test]
+		public void can_count_bits_in_a_long_with_efficient_method()
+		{
+			for (int i = 0; i < SampleCount; i++)
+			{
+				Console.WriteLine(cases[i].ToString("X") + " -> " + count[i]);
+				Assert.That(BitCounter.Count((ulong)cases[i]), Is.EqualTo(count[i]));
+			}
+		}
+	}
+}
